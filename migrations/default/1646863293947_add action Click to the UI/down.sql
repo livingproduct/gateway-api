e253949b119cf3ac98b31d2c5259a780de81b68b
chrispalmer <csp@getadministrate.com>
2022-03-09 22:33:04 +0000

    add action Click to the UI

---
+++ migrations/default/1646863293947_add action Click to the UI/down.sql
@@ -0,0 +1,12 @@
+
+alter table "public"."actionClick" alter column "accountId" drop not null;
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."actionClick" add column "accountId" text
+--  null;
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."flowActions" add column "accountId" text
+--  not null;
